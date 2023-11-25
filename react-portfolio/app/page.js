691ea637e57afe46825822fa77246c2ec4ee4ceb
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-11-25 16:00:58 +0100

    initialize project

---
+++ react-portfolio/app/page.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Head from "next/head"
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+
+      <Head>
+
+      </Head>
+
+      <main>
+        Hello world :(
+      </main>
+    </div>
+  )
+}
