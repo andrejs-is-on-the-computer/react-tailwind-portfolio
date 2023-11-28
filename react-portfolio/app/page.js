025de65632768f68601f9fe1e8a70f24d96fb577
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-11-28 18:51:18 +0100

    dark mode

--- react-portfolio/app/page.js
+++ react-portfolio/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import Head from "next/head"
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
@@ -25,26 +24,30 @@ export default function Home() {
       <Head>
 
       </Head>
+      <nav className="p-5 mb-12 flex justify-between fixed top-0 w-full shadow-md z-50 bg-teal-500 dark:bg-teal-800">
+        <h1 className="text-xl font-burtons dark:text-slate-100">developedbyChevvy</h1>
+        <ul className="flex items-center">
+          <li>
+          <div className="mx-auto rounded-full w-10 h-10 overflow-hidden cursor-pointer flex items-center justify-center bg-teal-800 dark:bg-teal-500" onClick={() => setDarkMode(!darkMode)}>
+            <BsFillMoonStarsFill className="text-2xl text-yellow-500 dark:text-yellow-500" />
+          </div>
+            
+          </li>
+          <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+        </ul>
+      </nav>
 
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="">
-          <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons">developedbyChevvy</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl" onClick={() => setDarkMode(!darkMode)} />
-              </li>
-              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
-            </ul>
-          </nav>
-          <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+          
+          <div className="text-center p-10 py-20">
+            <h2 className="text-5xl py-2 text-teal-500 font-medium md:text-6xl">
               Andrejs Bicevskis
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl">
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-slate-100">
               Developer and designer.
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-slate-400">
               Cool developer guy who definitely will not take your rejection of 
               my job application personally.
             </p>
@@ -67,17 +70,18 @@ export default function Home() {
         
         <section>
           <div>
-            <h3 className="text-3xl py-1">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
+            <h3 className="text-3xl py-1 dark:text-slate-100">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-400">
               I make the <span className="text-teal-500">website</span>. 
               I make it look <span className="text-teal-500">good</span>. 
               I make your <span className="text-teal-500">website</span> for you. 
               <span className="text-teal-500">Please</span>. Give me the job.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800">
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-400">
               Need money. Need Job. Need money. Need Job.
             </p>
           </div>
+
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
               <Image className="mx-auto" src={design} width={100} height={100} />
@@ -90,6 +94,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
             </div>
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
               <Image className="mx-auto" src={code} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
@@ -101,6 +106,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
             </div>
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
               <Image className="mx-auto" src={consulting} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
@@ -112,6 +118,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
             </div>
+
           </div>
         </section>
 
@@ -128,6 +135,7 @@ export default function Home() {
               Need money. Need Job. Need money. Need Job.
             </p>
           </div>
+
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap ">
             <div className="basis-1/3 flex-1">
               <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" />
@@ -148,6 +156,7 @@ export default function Home() {
               <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" />
             </div>
           </div>
+
         </section>
       </main>
     </div>
